13ab1ce1d4403e91d6984163b2fe0bc0cdb13fb3
yeazin <farsin310yeariha9701@gmail.com>
2021-04-09 20:16:15 +0600

    add views

--- crud/urls.py
+++ crud/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.HomeApp.as_view(), name= 'home' ),
     path('create_todo/', views.CreateTodoView.as_view(),name='create_todo'),
-    path('edit_todo/<int:id>/',views.EditTodoView.as_view(), name='edit')
+    path('edit_todo/<int:id>/',views.EditTodoView.as_view(), name='edit'),
+    path('delete_todo/<int:id>', views.DeleteTodoView.as_view(), name='delete')
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.core.paginator import Paginator
 from .models import ToDo
 
 
@@ -7,11 +8,14 @@ class HomeApp(View):
 
     def get(self, request, *args, **kwargs):
         todo_obj = ToDo.objects.all().order_by('-id')
+        paginator = Paginator(todo_obj, 9)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         context = {
-            'todo':todo_obj
+            'todo':page_obj
         }
         return render (request, 'index.html', context)
-
+        
 class CreateTodoView(View):
 
     def get(self, request, *args, **kwargs):
@@ -26,9 +30,15 @@ class CreateTodoView(View):
     
 class EditTodoView(View):
 
-
     def post(self, request,id,  *args, **kwargs):
         obj = get_object_or_404(ToDo, id=id )
         obj.list_name  = request.POST.get('todo')
         obj.save()
+        return redirect('home')
+
+class DeleteTodoView(View):
+
+    def post(self, request, id, *args, **kwargs):
+        obj = get_object_or_404(ToDo, id=id)
+        obj.delete()
         return redirect('home')
